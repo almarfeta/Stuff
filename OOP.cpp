efc6f71f500a3089f7e86bd0c38fd153249ab9e3
almarfeta <80604680+almarfeta@users.noreply.github.com>
2022-09-04 22:13:17 +0300

    Add files via upload

---
+++ OOP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Masca.h"
+#include "MascaChirurgicala.h"
+#include "MascaPolicarbonat.h"
+#include "Achizitie.h"
+
+using namespace std;
+
+int main()
+{
+    //Masca* m = new MascaChirurgicala("ffp1", "neagra", 13);
+    //MascaChirurgicala* m1 = (MascaChirurgicala*)m;
+    //cout << *m1; 
+    return 0;
+}
